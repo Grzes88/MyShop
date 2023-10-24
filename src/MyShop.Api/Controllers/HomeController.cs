b0930f4e7358dcd3e478dc5bec9e310e8424b6c5
Grzegorz Bączek <Gregor333000@gmail.com>
2023-10-24 21:55:28 +0200

    HomeController

---
+++ src/MyShop.Api/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using MyShop.Infrastructure;
+
+namespace MyShop.Api.Controllers
+{
+    [Route("")]
+    public class HomeController : ControllerBase
+    {
+        private readonly AppOptions _appOptions;
+
+        public HomeController(IOptions<AppOptions> appOptions)
+            => _appOptions = appOptions.Value;
+
+        [HttpGet]
+        public ActionResult Get() => Ok(_appOptions.Name);
+    }
+}
